9804a2f4d29e94ec9acdea164eedd32baa21ed31
YamaguchiHayato <167502303+YamaguchiHayato@users.noreply.github.com>
2025-07-23 11:06:21 +0900

    BGMの追加 (#2)

---
+++ Assets/BGM.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGMManager : MonoBehaviour
+{
+    public AudioSource audioSource; // AudioSourceをアタッチ
+    public AudioClip titleBGM;      // タイトル画面のBGM
+    public AudioClip playFirstBGM;  // 4ステージ目までのBGM
+    public AudioClip playSecondBGM; // 5～9ステージ目以降のBGM
+    public AudioClip resultBGM;     // リザルト画面のBGM
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);         // シーン間で音楽を保持
+        SceneManager.sceneLoaded += OnSceneLoaded;  // シーンロード時に呼ばれる
+    }
+
+    void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded; // メモリリーク防止
+    }
+
+    // シーンがロードされたときに呼ばれる
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        switch (scene.name)
+        {
+            // ケース:タイトル
+            case "Title":
+                ChangeBGM(titleBGM);
+                break;
+            // ケース:プレイ
+            case "Play":
+                ChangeBGM(playFirstBGM);
+                break;
+            // ケース:プレイ（5～9ステージ目）
+            case "PlaySecond":
+                ChangeBGM(playSecondBGM);
+                break;
+            // ケース:リザルト
+            case "Result":
+                ChangeBGM(resultBGM);
+                break;
+        }
+    }
+
+    // BGMを変更するメソッド
+    void ChangeBGM(AudioClip clip)
+    {
+        if (audioSource.clip != clip)
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+    }
+}

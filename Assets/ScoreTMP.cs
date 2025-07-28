d7ab871a062061e3a122d5471cf4459bfc64d4c6
伊藤侑生 <ito3150ito@gmail.com>
2025-07-28 16:20:07 +0900

    リザルトのスコア処理実装

--- Assets/ScoreTMP.cs
+++ Assets/ScoreTMP.cs
@@ -19,6 +19,42 @@ public class ScoreTMP : MonoBehaviour
         Score += AddScore; // スコアを加算
     }
 
+    // スコアをリセットするメソッドを追加
+    public void ResetScore()
+    {
+        Score = 0;
+    }
+
+    //// スコアを読み込むメソッド
+    //public void LoadScore()
+    //{
+    //    Score = PlayerPrefs.GetInt("Score", 0);
+    //}
+
+    // ゴール番号を指定してスコアを保存
+    public void SaveScoreForGoal(int goalNumber)
+    {
+        PlayerPrefs.SetInt($"Score_Goal_{goalNumber}", Score);
+        PlayerPrefs.Save();
+    }
+
+    // ゴール番号を指定してスコアを取得
+    public int LoadScoreForGoal(int goalNumber)
+    {
+        return PlayerPrefs.GetInt($"Score_Goal_{goalNumber}", 0);
+    }
+
+    // 合計スコアを取得
+    public int LoadTotalScore()
+    {
+        int total = 0;
+        for (int i = 1; i <= 5; i++)
+        {
+            total += LoadScoreForGoal(i);
+        }
+        return total;
+    }
+
     // Update is called once per frame
     void Update()
     {
